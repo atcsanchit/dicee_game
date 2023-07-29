b7cddc774399d77a6bc68529c07be47981fba542
atcsanchit <137472694+atcsanchit@users.noreply.github.com>
2023-07-29 09:52:09 +0530

    Update first.js

--- first.js
+++ first.js
@@ -1,7 +1,7 @@
 var d1=Math.floor((Math.random()*5)+1);
 var d2=Math.floor((Math.random()*5)+1);
 
-var address=["images/dice1.png","images/dice2.png","images/dice3.png","images/dice4.png","images/dice5.png","images/dice6.png"];
+var address=["dice1.png","dice2.png","dice3.png","dice4.png","dice5.png","dice6.png"];
 
 document.querySelector(".img1").setAttribute("src",address[d1-1]);
 document.querySelector(".img2").setAttribute("src",address[d2-1]);
